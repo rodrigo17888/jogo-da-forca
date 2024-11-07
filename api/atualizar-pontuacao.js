1a5b38273af12ba3d9b29bcc5f2e41fdd77f340d
rodrigo17888 <rodrigosilvalopes16@gmail.com>
2024-11-06 23:32:57 -0300

    Conectando ao banco de dados

--- api/atualizar-pontuacao.js
+++ api/atualizar-pontuacao.js
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Configuração do Supabase
 const supabase = createClient(
-  "https://mnakiqkfkuqdnlsvipyq.supabase.co", // URL do Supabase
-  "your-anon-key" // Chave de API pública (anon key)
+  "hhttps://eliwdfrelzhtzdagibno.supabase.co", // URL do Supabase
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsaXdkZnJlbHpodHpkYWdpYm5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5MzA4MjMsImV4cCI6MjA0NjUwNjgyM30.CMuNNsTc8uufiKpccAv4-n5AdTdij8bccX7Gbh6HsjU" // Chave de API pública (anon key)
 );
 
 export default async function handler(req, res) {
@@ -24,12 +24,10 @@ export default async function handler(req, res) {
 
       res.status(200).json({ message: "Pontuação salva com sucesso!" });
     } catch (error) {
-      res
-        .status(500)
-        .json({
-          error: "Erro ao atualizar a pontuação",
-          details: error.message,
-        });
+      res.status(500).json({
+        error: "Erro ao atualizar a pontuação",
+        details: error.message,
+      });
     }
   } else {
     res.status(405).json({ message: "Método não permitido" });
--- api/ranking.js
+++ api/ranking.js
@@ -3,29 +3,22 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://mnakiqkfkuqdnlsvipyq.supabase.co", // URL do Supabase
-  "your-anon-key" // Chave de API pública
+  "hhttps://eliwdfrelzhtzdagibno.supabase.co", // URL do Supabase
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsaXdkZnJlbHpodHpkYWdpYm5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5MzA4MjMsImV4cCI6MjA0NjUwNjgyM30.CMuNNsTc8uufiKpccAv4-n5AdTdij8bccX7Gbh6HsjU" // Chave de API pública (anon key)
 );
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    try {
-      // Consulta o ranking do banco de dados
-      const { data, error } = await supabase
-        .from("ranking")
-        .select("*")
-        .order("pontuacao", { ascending: false }); // Ordena pela pontuação
+    const { data, error } = await supabase.from("ranking").select("*");
 
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      res.status(200).json(data);
-    } catch (error) {
+    if (error) {
       res
         .status(500)
-        .json({ error: "Erro ao carregar o ranking", details: error.message });
+        .json({ error: "Erro ao carregar o ranking", details: error });
+      return;
     }
+
+    res.status(200).json(data);
   } else {
     res.status(405).json({ message: "Método não permitido" });
   }
